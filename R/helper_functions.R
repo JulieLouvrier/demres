f3b0e8105bdf0b419a265957dcadcd746b7d980c
Julie Louvrier <34424972+JulieLouvrier@users.noreply.github.com>
2024-04-29 19:00:43 +0200

    helper functions documented and hidden

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -2,13 +2,6 @@
 #'
 #' \code{RMSE} Calculates RMSE between the time-varying resilience
 #' metric and the time-constant one
-#'
-#'#' \code{rRMSE} Calculates rRMSE between the time-varying resilience
-#' metric and the time-constant one
-#'
-#' #' \code{MAPE} Calculates MAPE between the time-varying resilience
-#' metric and the time-constant one
-#'
 #' @param TV A vector containing values of time-varying demographic resilience
 #' for one specific  metric, calculated with demres
 #' @param TC A vector containing the value of the time-constant demographic
@@ -23,9 +16,36 @@ RMSE <- function(TV, TC) {
   sqrt(mean((TV - TC) ^ 2, na.rm = T))
 }
 
+
+#' Helper functions for demres_dist function from the package "demres"
+#'
+#' \code{rRMSE} Calculates rRMSE between the time-varying resilience
+#' metric and the time-constant one
+#' @param TV A vector containing values of time-varying demographic resilience
+#' for one specific  metric, calculated with demres
+#' @param TC A vector containing the value of the time-constant demographic
+#'  resilience for the same one specific  metric as for TV, calculated with demres
+#' @return Values of the distance measure
+#' @export
+#' @name helper_functions
+#' @keywords internal
+
 rRMSE <- function(TV, TC) {
   sqrt(mean((TV - TC) ^ 2, na.rm = T)) / sd(TV, na.rm = T)
 }
+
+#' Helper functions for demres_dist function from the package "demres"
+#'
+#' \code{MAPE} Calculates MAPE between the time-varying resilience
+#' metric and the time-constant one
+#' @param TV A vector containing values of time-varying demographic resilience
+#' for one specific  metric, calculated with demres
+#' @param TC A vector containing the value of the time-constant demographic
+#'  resilience for the same one specific  metric as for TV, calculated with demres
+#' @return Values of the distance measure
+#' @export
+#' @name helper_functions
+#' @keywords internal
 
 MAPE <- function(TV, TC) {
   mean(abs(TV - TC), na.rm = T)
