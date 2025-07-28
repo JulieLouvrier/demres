69c92cc6480bd8f5aa8d238beba0edd43b520553
Cédric Scherer <cedricphilippscherer@gmail.com>
2025-07-28 17:20:18 +0200

    showcase latest functionality

--- inst/new_functionality_cedric.R
+++ inst/new_functionality_cedric.R
@@ -4,9 +4,9 @@ load("./data/adeliepenguin.rda")
 library(popdemo)
 library(ggplot2)
 
-set.seed(123456)
+set.seed(1234)
 
-# pop vect generated an not standardized
+# pop vect generated (not standardized)
 penguinvec1 <- round(runif(2) * 100)
 
 
@@ -21,42 +21,51 @@ peng_proj_std <- popdemo::project(
 )
 
 
-# absolute
+# absolute .....................................................................
+
+## "original" plot
 popdemo::plot(
   peng_proj_reg,
   ylim = c(0, 300)
 )
 
-plot_results(
-    popvec = peng_proj_reg,
-    standard.A = FALSE # default
-  )
+# default for a single (non-standardized) projection:
+plot_proj(popvec = peng_proj_reg)
 
-plot_results(
-    popvec = peng_proj_reg,
-    standard.A = FALSE # default
-  ) +
+# if you liek to adjust limits, use regular ggplot logic:
+plot_proj(popvec = peng_proj_reg) +
   coord_cartesian(ylim = c(0, 300))
 
-plot_results(
-    popvec = peng_proj_reg,
-    palette = "red",
-    sort = TRUE, # will be ignored
-    facet = TRUE, # will be ignored
-    compare = TRUE  # will be ignored
-  )
+# all parameters -> those which are not applicable return message
+plot_proj(
+  popvec = peng_proj_reg,
+  standard.A = FALSE, # default
+  palette = "red",
+  sort = TRUE, # will be ignored
+  facet = TRUE, # will be ignored
+  compare = TRUE  # will be ignored
+)
 
 
-# standardized
+# standardized .................................................................
+
+## "original" plot
 popdemo::plot(
   peng_proj_std,
   ylim = c(0, 100)
 )
 
-plot_results(
+plot_proj(
+  popvec = peng_proj_std,
+  standard.A = TRUE # overwrite default
+)
+
+plot_proj(
     popvec = peng_proj_std,
-    standard.A = TRUE
-  )
+    standard.A = TRUE, # overwrite default
+    palette = "royalblue"
+  ) +
+  coord_cartesian(ylim = c(0, 100))
 
 
 ## MULTIPLE POPULATIONS --------------------------------------------------------
@@ -73,87 +82,92 @@ pops_proj_std <- lapply(c(1:length(adeliepenguin)), FUN = function( x ) {(
   )
 )})
 
-my_colors <- viridis::mako(n = length(pops_proj_std), begin = .1, end = .8)
+my_colors <- viridis::rocket(n = length(pops_proj_std), begin = .1, end = .8)
 
-# absolute
-# popdemo::plot(
-#   pops_proj_reg,
-#   ylim = c(0, 300)
-# )
+my_colors_named <- my_colors
+names(my_colors_named) <- 1:28
 
-plot_results(
-  popvec = pops_proj_reg
-)
+# absolute .....................................................................
 
-plot_results(
+## function detects if multiple years are passed
+## -> now uses facet and compare as defaults
+plot_proj(popvec = pops_proj_reg)
+
+## if you like to add color nevertheless:
+plot_proj(
   popvec = pops_proj_reg,
   palette = my_colors
 )
 
-plot_results(
+## if you don't like the comparison lines:
+plot_proj(
   popvec = pops_proj_reg,
-  facet = TRUE
+  compare = FALSE
 )
 
-plot_results(
+## if you'd like to get all in a single plot:
+## -> now with the blue shades as default
+plot_proj(
   popvec = pops_proj_reg,
-  facet = TRUE,
-  compare = TRUE
+  facet = FALSE
 )
 
-plot_results(
+## we can rank the lines (note that colors now encode the ranks now, not the
+## time steps in increasing order anymore - use custom named vector if you
+## wish to do so, example below)
+plot_proj(
   popvec = pops_proj_reg,
-  facet = TRUE,
-  compare = TRUE,
-  palette = my_colors
+  facet = FALSE,
+  sort = TRUE
 )
 
-plot_results(
+## use custom color palette (unnamed vector):
+plot_proj(
   popvec = pops_proj_reg,
-  facet = TRUE,
+  facet = FALSE,
   sort = TRUE,
-  compare = TRUE,
   palette = my_colors
 )
 
-# standardized
-# popdemo::plot(
-#   pops_proj_std,
-#   ylim = c(0, 100)
-# )
+## named vector instead to keep original sorting by time step:
+plot_proj(
+  popvec = pops_proj_reg,
+  facet = FALSE,
+  sort = TRUE,
+  palette = my_colors_named
+)
 
-plot_results(
-    popvec = pops_proj_std,
-    standard.A = TRUE
-  )
 
-plot_results(
-    popvec = pops_proj_std,
-    standard.A = TRUE,
-    palette = my_colors
-  )
+## we can sort them in the facet view as well:
+plot_proj(
+  popvec = pops_proj_reg,
+  sort = TRUE,
+  palette = my_colors_named
+)
 
-plot_results(
-    popvec = pops_proj_std,
-    standard.A = TRUE,
-    facet = TRUE
-  )
+plot_proj(
+  popvec = pops_proj_reg,
+  sort = TRUE,
+  palette = "red"
+)
 
-plot_results(
-    popvec = pops_proj_std,
-    standard.A = TRUE,
-    facet = TRUE,
-    compare = TRUE
-  )
 
-plot_results(
-    popvec = pops_proj_std,
-    standard.A = TRUE,
-    facet = TRUE,
-    compare = TRUE,
-    palette = my_colors
-  )
+# standardized .................................................................
 
+plot_proj(
+  popvec = pops_proj_std,
+  sort = TRUE
+)
+
+plot_proj(
+  popvec = pops_proj_std,
+  facet = FALSE,
+  sort = TRUE
+)
+
+
+
+## for labeling (ignore for now)
 
 vector_list <- lapply(1:28, function(i) penguinvec1)
 
