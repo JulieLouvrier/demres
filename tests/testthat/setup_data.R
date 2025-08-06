1c599b1c932eee97afb891f3f9a5fc14c0c0c1a2
Julie Louvrier <34424972+JulieLouvrier@users.noreply.github.com>
2025-08-06 15:56:10 +0200

    Updated the setup_data for testthat

--- tests/testthat/setup_data.R
+++ tests/testthat/setup_data.R
@@ -1,6 +1,6 @@
 data(adeliepenguin)
 
 # simulate an initial vector
-set.seed(1234)
-penguinvec1 <- runif(5)
-penguinvec1 <- penguinvec1/sum(penguinvec1) #scales the vector to sum up to 1
+set.seed(125435)
+vec <- round(runif(2) * 100 )
+vector_list <- lapply(1:28, function(i) vec)
